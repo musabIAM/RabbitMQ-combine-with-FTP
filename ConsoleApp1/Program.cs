9d3732ffd9250161af02b45a5e19f9fa096a0e48
Mus'ab Ibrahim Adan Maalin <41560084+musabIAM@users.noreply.github.com>
2025-04-18 10:39:51 +0700

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json.Linq;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory();
+            factory.UserName = "eaglevsandbox";
+            factory.Password = "S4nboxv";
+            factory.VirtualHost = "/eaglevsandbox";
+            factory.HostName = "rmq1.pptik.id";
+            factory.Port = 5672;
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "yolo",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    //JObject obj = JObject.Parse(message);
+                    var path = @"C:\Users\ASUS\Desktop\"+message;
+                    downloadftp(path,message);
+
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(queue: "yolo",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+        private static bool downloadftp(String path,String filename)
+        {
+            try
+            {
+
+                var FTPHost = "ftp://eaglevsandboxftp.pptik.id/";
+                var FTPResultDirectory = "eaglev_sandbox/";
+                var FTPUsername = "eaglevsandbox";
+                var FTPPassword = "S4nboxv";
+
+                Console.WriteLine("Download :" + FTPHost + FTPResultDirectory + filename);
+                WebClient request = new WebClient();
+               
+                //FtpWebRequest request = (FtpWebRequest)WebRequest.Create(FTPHost + FTPResultDirectory + filename);
+                //request.Method = WebRequestMethods.Ftp.DownloadFile;
+                //request.UseBinary = true;
+                //request.UsePassive = true;
+                //request.KeepAlive = true;
+                request.Credentials = new NetworkCredential(FTPUsername, FTPPassword);
+                byte[] fileData = request.DownloadData(FTPHost + FTPResultDirectory + filename);
+
+                //FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+                //Stream responsetStream = response.GetResponseStream();
+
+                using (FileStream file = File.Create(path))
+                {
+                    file.Write(fileData, 0, fileData.Length);
+                    file.Close();
+                  
+                    Console.WriteLine("Success Download FTP");
+                }
+
+                    return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed Download FTP");
+                Console.WriteLine(e);
+                return false;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,103 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\ASUS\Pictures\kucing-anjing.jpg";
+            var fname = "kucing-anjing.jpg";
+
+        var factory = new ConnectionFactory();
+            factory.UserName = "aa";
+            factory.Password = "aa";
+            factory.VirtualHost = "/aa";
+            factory.HostName = "rmq1.aa.id";
+            factory.Port = 5672;
+            using (var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "yolo",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+          
+            var message = fname;
+            var body = Encoding.UTF8.GetBytes(message);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "yolo",
+                                 basicProperties: properties,
+                                 body: body);
+            Console.WriteLine("Sent ", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+
+
+            uploadtoftp(path, fname);
+        }
+
+        //private static string GetMessage(string[] args)
+        //{
+            //return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        //}
+
+        private static bool uploadtoftp(string localfile, String filename)
+        {
+            try
+            {
+
+                var FTPHost = "ftp://eaglevsandboxftp.pptik.id/";
+                var FTPResultDirectory = "eaglev_sandbox/";
+                var FTPUsername = "eaglevsandbox";
+                var FTPPassword = "S4nboxv";
+
+                Console.WriteLine("Uploading file to :" + FTPHost + FTPResultDirectory + filename);
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(FTPHost + FTPResultDirectory + filename);
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+                request.UseBinary = true;
+                request.UsePassive = true;
+                request.KeepAlive = true;
+                request.Credentials = new NetworkCredential(FTPUsername, FTPPassword);
+
+
+                using (FileStream fs = File.OpenRead(@localfile))
+                {
+                    byte[] buffer = new byte[fs.Length];
+                    fs.Read(buffer, 0, buffer.Length);
+                    fs.Close();
+                    Stream requestStream = request.GetRequestStream();
+                    requestStream.Write(buffer, 0, buffer.Length);
+                    requestStream.Flush();
+                    requestStream.Close();
+                    FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+                    Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription);
+                    response.Close();
+                    Console.WriteLine("Success Upload File to FTP");
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed Upload File to FTP");
+                Console.WriteLine(e);
+                return false;
+            }
+
+        }
+    }
+}
